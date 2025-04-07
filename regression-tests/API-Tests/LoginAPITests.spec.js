0af27eeaed3eceb5a905c727b7a7439b482476f7
joelandrews614 <joelandrews614@gmail.com>
2025-04-07 10:47:18 +0530

    Moved API Collections File to the Test Folder

--- regression-tests/API-Tests/LoginAPITests.spec.js
+++ regression-tests/API-Tests/LoginAPITests.spec.js
@@ -4,7 +4,7 @@ import { execSync } from 'child_process';
 
 test("Validation of Jules AI Collections using Newman", async () => {
 
-    const result = execSync('npx newman run test-data\\Jules_API_Collection.postman_collection.json').toString();
+    const result = execSync('npx newman run Jules_API_Collection.postman_collection.json').toString();
     console.log(result);
 
 })
