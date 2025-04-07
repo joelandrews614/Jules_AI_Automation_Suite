d005594baaa4381d8cfa839fac5e1152da469389
joelandrews614 <joelandrews614@gmail.com>
2025-04-07 15:24:31 +0530

    Commited

--- regression-tests/API-Tests/LoginAPITests.spec.js
+++ regression-tests/API-Tests/LoginAPITests.spec.js
@@ -2,7 +2,7 @@ import {test, expect} from "@playwright/test"
 import { execSync } from 'child_process';
 
 
-test("Validation of Jules AI Collections using Newman", async () => {
+test.skip("Validation of Jules AI Collections using Newman", async () => {
 
     const result = execSync('npx newman run Regression-Tests/API-Tests/Jules_API_Collection.postman_collection.json').toString();
     console.log(result);
